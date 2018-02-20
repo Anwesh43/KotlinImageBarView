b23b758474e91431d7c359763ab523b929823842
Anwesh43 <anweshthecool0@gmail.com>
2018-02-20 08:24:30 +0530

    adding fullscreen support

--- app/src/main/java/ui/anwesome/com/kotlinimagebarview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinimagebarview/MainActivity.kt
@@ -3,6 +3,7 @@ package ui.anwesome.com.kotlinimagebarview
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Toast
 import ui.anwesome.com.imagebarview.ImageBarView
 
@@ -16,5 +17,10 @@ class MainActivity : AppCompatActivity() {
         }, {
             Toast.makeText(this, "image destroyed", Toast.LENGTH_SHORT).show()
         })
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
