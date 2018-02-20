aee47865cbe8c4ff57fa79649d030da41e18037c
Anwesh43 <anweshthecool0@gmail.com>
2018-02-20 08:09:35 +0530

    adding settings

--- app/src/main/java/ui/anwesome/com/kotlinimagebarview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinimagebarview/MainActivity.kt
@@ -1,12 +1,14 @@
 package ui.anwesome.com.kotlinimagebarview
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.imagebarview.ImageBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ImageBarView.create(this, BitmapFactory.decodeResource(resources, R.drawable.nature_more))
     }
 }
