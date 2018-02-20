528381405426f21204348116c5d6640b211db899
Anwesh43 <anweshthecool0@gmail.com>
2018-02-20 07:31:54 +0530

    adding ImageBarState

--- imagebarview/src/main/java/ui/anwesome/com/imagebarview/ImageBarView.kt
+++ imagebarview/src/main/java/ui/anwesome/com/imagebarview/ImageBarView.kt
@@ -7,7 +7,7 @@ import android.content.*
 import android.view.*
 import android.graphics.*
 
-class ImageBarView(ctx:Context):View(ctx) {
+class ImageBarView(ctx:Context, var bitmap:Bitmap):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
 
@@ -17,5 +17,22 @@ class ImageBarView(ctx:Context):View(ctx) {
 
         }
         return true
+    }
+    data class ImageBarState(var scale:Float = 0f, var dir:Float = 0f, var prevScale:Float = 0f) {
+        fun update(stopcb : (Float) -> Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if(dir == 0f) {
+                dir = 1f - 2 * scale
+                startcb()
+            }
+        }
     }
 }
