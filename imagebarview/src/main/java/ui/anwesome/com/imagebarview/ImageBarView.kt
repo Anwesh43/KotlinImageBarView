82c2dd48e22859e2e8b9d7d4548612f805a32286
Anwesh43 <anweshthecool0@gmail.com>
2018-02-20 07:34:01 +0530

    adding Animator class

--- imagebarview/src/main/java/ui/anwesome/com/imagebarview/ImageBarView.kt
+++ imagebarview/src/main/java/ui/anwesome/com/imagebarview/ImageBarView.kt
@@ -35,4 +35,29 @@ class ImageBarView(ctx:Context, var bitmap:Bitmap):View(ctx) {
             }
         }
     }
+    data class Animator(var view:View, var animated:Boolean = false) {
+        fun animate(updatecb : () -> Unit) {
+            if(animated) {
+                updatecb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch(ex:Exception) {
+
+                }
+            }
+        }
+        fun start() {
+            if(!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+        fun stop() {
+            if(animated) {
+                animated = false
+            }
+        }
+    }
 }
